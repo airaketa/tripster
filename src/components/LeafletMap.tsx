0d5ea8429e65ba57da875c8040705e5e658ca150
airaketa <aleksei.airapetov@yandex.ru>
2020-11-24 01:30:35 +0300

    update leaflet css and js hash sums

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -3,17 +3,17 @@ import { MapContainer } from 'react-leaflet';
 import { TileLayer } from 'react-leaflet';
 import { LatLngTuple } from 'leaflet';
 
-const defaultLatLng: LatLngTuple = [48.865572, 2.283523];
+const position: LatLngTuple = [48.865572, 2.283523];
 const zoom:number = 8;
 
 const LeafletMap:React.FC = () => {
     return (
         <MapContainer id="mainMap"
-                center={defaultLatLng}
+                center={position}
                 zoom={zoom}>
             <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
         </MapContainer>
         
